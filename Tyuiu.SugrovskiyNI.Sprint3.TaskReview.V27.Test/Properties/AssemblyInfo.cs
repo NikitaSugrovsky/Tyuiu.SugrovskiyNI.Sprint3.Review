ad6cf67d7328d85cf832ff91250120ebb3fe1066
Nikita Sugrovsky <sugrovskiy01@bk.ru>
2023-10-31 17:06:25 +0500

    Add project files.

---
+++ Tyuiu.SugrovskiyNI.Sprint3.TaskReview.V27.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.SugrovskiyNI.Sprint3.TaskReview.V27.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.SugrovskiyNI.Sprint3.TaskReview.V27.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("38750d4d-3d18-4169-944e-0ae982037aa6")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Tyuiu.SugrovskiyNI.Sprint3.TaskReview.V27/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.SugrovskiyNI.Sprint3.TaskReview.V27.Lib;
+
+namespace Tyuiu.SugrovskiyNI.Sprint3.TaskReview.V27
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #3 | Выполнил: Сугровский Н. И. | ИИПб-23-1";
+            Console.WriteLine("******************************************************************************");
+            Console.WriteLine("* Спринт #3                                                                  *");
+            Console.WriteLine("* SprintReview                                                               *");
+            Console.WriteLine("* Задание #7                                                                 *");
+            Console.WriteLine("* Вариант #27                                                                *");
+            Console.WriteLine("* Выполнил: Сугровский Никита Игоревич | ИИПб-23-1                           *");
+            Console.WriteLine("******************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                   *");
+            Console.WriteLine("* Написать программу,которая выводит таблицу значения функций                *");
+            Console.WriteLine("* (произвести табулирование) f(x) на заданном диапазоне  [-5;5]              *");
+            Console.WriteLine("*                                                                            *");
+            Console.WriteLine("******************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                           *");
+            Console.WriteLine("******************************************************************************");
+
+            int startValue = -5;
+            int stoptValue = 5;
+
+            Console.WriteLine("Старт шага = " + startValue);
+            Console.WriteLine("Конец шага = " + stoptValue);
+
+            int len = ds.GetMassFunction(startValue, stoptValue).Length;
+
+            double[] valueArray;
+            valueArray = new double[len];
+
+            valueArray = ds.GetMassFunction(startValue, stoptValue);
+
+
+            Console.WriteLine("******************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                                 *");
+            Console.WriteLine("******************************************************************************");
+
+            Console.WriteLine("+---------------------------------------+-------------------------------------");
+            Console.WriteLine("*|                  X                   |                  f(X)              |");
+            Console.WriteLine("+---------------------------------------+-------------------------------------");
+            for (int i = 0; i <= len - 1; i++)
+            {
+                Console.WriteLine("|{0,5:d}                                  | {1, 6:f2}                             |", startValue, valueArray[i]);
+                startValue++;
+            }
+            Console.WriteLine("+---------------------------------------+------------------------------------+");
+            Console.ReadKey();
+        }
+    }
+}
